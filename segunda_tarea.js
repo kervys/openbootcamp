98077263821ec8e32a8d944b9061417b1d9cd691
unknown <kervysjulian@gmail.com>
2022-09-05 17:36:57 -0400

    segunda tarea

---
+++ segunda_tarea.js
@@ -0,0 +1,13 @@
+const datosPersonales = {
+    nombre: 'Kervys Arguello',
+    edad: 46,
+    desarrollador: false,
+    fechaNacimiento: '06/19/1976',
+    librofavorito: {
+        titulo: 'Proyeco Node',
+        autor: 'Kervys',
+        fecha: '02/14/2015',
+        url: 'http://librosdetodos.com/'}
+}
+
+console.log(datosPersonales);
